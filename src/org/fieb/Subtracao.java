bcba8c623b58cb3354146d86cdb27f9b653d2aa6
felipesncf <piocarvalho_1@hotmail.com>
2019-04-23 19:42:22 -0300

    Foi adicionado a funcao de multiplicacao

---
+++ src/org/fieb/Subtracao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.fieb;
+
+/**
+ *
+ * @author pioca_000
+ */
+public class Subtracao {
+    public static double calcular(double x, double y){
+        return x-y;
+    }
+}
