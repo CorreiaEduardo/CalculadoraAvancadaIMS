4cd38f4f55d6fd02b14c99a222c12a42c8c3846e
Felipe Prado Bastos <felipepradobastos@gmail.com>
2019-04-24 20:43:23 -0300

    Corrigindo Bug String final

--- src/org/fieb/Program.java
+++ src/org/fieb/Program.java
@@ -20,19 +20,20 @@ public class Program {
                 String[] parts = parts = equacao.split(" ");
                 double num1 = Integer.parseInt(parts[0]);
                 double num2 = Integer.parseInt(parts[2]);
+                String operator = parts[1];
                 
-                if (equacao.contains("+")) {
+                if (operator.equals("+")) {
                     System.out.println("O resultado da soma é "+Soma.calcular(num1, num2));
-                }else if(equacao.contains("-")){
+                }else if(operator.equals("-")){
                     System.out.println("O resultado da subtração é "+Subtracao.calcular(num1, num2));
-                }else if(equacao.contains("*")){
+                }else if(operator.equals("*")){
                     System.out.println("O resultado da multiplicacao é "+ Multiplicacao.calculo(num1, num2));
-                }else if(equacao.contains("/")){
+                }else if(operator.equals("/")){
                     System.out.println("O resultado da divisão é "+Divisao.calcular(num1, num2));
-                }else if(equacao.contains("l")){
+                }else if(operator.equalsIgnoreCase("l")){
                     System.out.println("O resultado do logaritimo é "+Logaritimo.calcular(num1, num2));
                 }   
-                else if(equacao.contains("%")){
+                else if(operator.equals("%")){
                     System.out.println("O resultado da porcentagem é "+Porcentagem.calcular(num1, num2));
                 }
             } catch (NullPointerException ex) {
