1cb88a1728e9e35b723e01daf0b8d0ed71f573b7
felipesncf <piocarvalho_1@hotmail.com>
2019-04-23 21:19:37 -0300

    Foi corrigido a funcao da multiplicacao

---
+++ src/org/fieb/Multiplicacao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.fieb;
+
+/**
+ *
+ * @author pioca_000
+ */
+public class Multiplicacao {
+    public static double calculo(double x, double y){
+        return x*y;
+    }
+}
